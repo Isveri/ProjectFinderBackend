39f90395c820e6d3028f69afe957026425522c1c
Evistix <mephistakaiwan@gmail.com>
2022-08-08 13:54:44 +0200

    mess fix for eventlisteners

--- src/main/java/com/example/project/chat/controller/WebSocketEventListener.java
+++ src/main/java/com/example/project/chat/controller/WebSocketEventListener.java
@@ -28,45 +28,66 @@ public class WebSocketEventListener {
     private Map<String,Map<String,Long>> connectedUsers = new HashMap<>();
 //TODO mozna cos zrobic z ta mapa connectedUsers bo trzymanie tego w pamieci 2/10
 
-    //TODO POPRAWIC WYYGLAD
     @EventListener
     public void handleSessionSubscribe(SessionSubscribeEvent event) {
+
         SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
         String username = headers.getUser().getName();
+
+        String id = getGroupIdFromHeader(headers);
+
+        Principal principal =  headers.getUser();
+        User user = (User) (principal != null ? ((UsernamePasswordAuthenticationToken) principal).getPrincipal() : null);
+
+        Map<String,Long> users = getMapById(id) ;
+
+        users.put(headers.getSessionId(),user.getId());
+        connectedUsers.put(id,users);
+        Set<Long> usersIdToSend = getIds(id);
+
+        createMessage(usersIdToSend, username, "joined",id);
+    }
+    @EventListener
+    public void handleSessionDisconnect(SessionDisconnectEvent event) {
+
+        SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
+        String username = headers.getUser().getName();
+
+        String id = destination.get(event.getSessionId());
+
+        connectedUsers.get(id).remove(headers.getSessionId());
+
+        Set<Long> usersIdToSend = getIds(id);
+
+        createMessage(usersIdToSend, username, "left",id);
+
+    }
+
+    private Set<Long> getIds(String id) {
+        Set<Long> usersIdToSend = new HashSet<>();
+        for(Map.Entry<String,Long> entry: connectedUsers.get(id).entrySet()){
+            usersIdToSend.add(entry.getValue());
+        }
+        return usersIdToSend;
+    }
+
+    private String getGroupIdFromHeader(SimpMessageHeaderAccessor headers){
         List<String> idList = headers.getNativeHeader("destination");
         String[] splited = idList.get(0).split("/");
         String id = splited[splited.length - 1];
         destination.put(headers.getSessionId(), id);
-        Principal principal =  headers.getUser();
-        User user = (User) (principal != null ? ((UsernamePasswordAuthenticationToken) principal).getPrincipal() : null);
-
-        Map<String,Long> users;
-        if(connectedUsers.get(id)==null){
-             users = new HashMap<>();
-        }else {
-             users = connectedUsers.get(id);
-        }
-        users.put(headers.getSessionId(),user.getId());
-        connectedUsers.put(id,users);
-        Set<Long> usersIdToSend = new HashSet<>();
-        for(Map.Entry<String,Long> entry: connectedUsers.get(id).entrySet()){
-            usersIdToSend.add(entry.getValue());
-        }
-        MessageDTO messageDTO = MessageDTO.builder().text(username + " joined chat").connectedUsers(new ArrayList<>(usersIdToSend)).build();
-        messagingTemplate.convertAndSend("/topic/messages/"+id,messageDTO);
+        return id;
     }
 
-    @EventListener
-    public void handleSessionDisconnect(SessionDisconnectEvent event) {
-        SimpMessageHeaderAccessor headers = SimpMessageHeaderAccessor.wrap(event.getMessage());
-        String username = headers.getUser().getName();
-        String id = destination.get(event.getSessionId());
-        connectedUsers.get(id).remove(headers.getSessionId());
-        Set<Long> usersIdToSend = new HashSet<>();
-        for(Map.Entry<String,Long> entry: connectedUsers.get(id).entrySet()){
-            usersIdToSend.add(entry.getValue());
+    private Map<String,Long> getMapById(String id){
+        if(connectedUsers.get(id)==null){
+             return new HashMap<>();
         }
-        MessageDTO messageDTO = MessageDTO.builder().text(username + " left chat").connectedUsers(new ArrayList<>(usersIdToSend)).build();
+        return connectedUsers.get(id);
+    }
+
+    private void createMessage(Set<Long> usersIdToSend,String username, String value, String id){
+        MessageDTO messageDTO = MessageDTO.builder().text(username +" "+value+" chat").connectedUsers(new ArrayList<>(usersIdToSend)).build();
         messagingTemplate.convertAndSend("/topic/messages/"+id,messageDTO);
     }
 }
